b03eb7f8087b18921889a99f79669bd3e9a11390
Alex Gerhardt <alex.gerhardt@caylent.com>
2026-03-24 12:46:22 -0400

    server setup

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/disconnectedag/go-server
+
+go 1.26.1
---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+type Server struct {}
+
+func main() {
+	const port = "8080"
+
+	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(".")))
+
+	s := &http.Server{Addr: ":" + port, Handler: mux}
+	log.Printf("Serving on port: %s\n", port)
+	log.Fatal(s.ListenAndServe())
+}
